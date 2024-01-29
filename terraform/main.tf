aeb28a2a2989327a8a1cdf1f902bc5cd320c82b5
KirillRapoport <likirsax@gmail.com>
2024-01-29 15:52:46 +0300

    First commit

---
+++ terraform/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "sg_otus_reddit_app" {
+  name        = "SG for otus reddit app"
+dynamic "ingress" { 
+  for_each = [ "80", "22", "443" , "9292" ] 
+  content {
+    from_port        = ingress.value
+    to_port          = ingress.value
+    protocol         = "tcp" 
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+  egress {
+    from_port        = 0 
+    to_port          = 0 
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "WebServer_for_otus_reddit_app" {
+  ami                    = "ami-04d4b63992fdf183e"
+  instance_type          = "t2.micro"
+  key_name               = "toolkir"
+  vpc_security_group_ids = [aws_security_group.sg_otus_reddit_app.id]
+}
+
+output "instance_public_ip" {
+  value = aws_instance.WebServer_for_otus_reddit_app.public_ip
+} 
